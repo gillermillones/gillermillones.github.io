25db5000331aa2435fad4f98ee9efb53e8e4ec30
gillermillones <titicht@hotmail.com>
2025-12-11 20:32:13 +0100

    sw funcionando mejor

--- app/sw.js
+++ app/sw.js
@@ -1,30 +1,38 @@
 importScripts('/workbox-sw.js');
 
-self.addEventListener('install', event => self.skipWaiting());
-self.addEventListener('activate', event => clients.claim());
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', () => clients.claim());
 
-workbox.precaching.precacheAndRoute(self.__WB_MANIFEST || []);
+let precacheManifest = self.__WB_MANIFEST || [];
+
+precacheManifest = precacheManifest.filter(entry => !entry.url.startsWith('.next/static/'));
+
+precacheManifest = precacheManifest.map(entry => ({
+  entry,
+  url: entry.url.replace(/^public\//, '/')
+}));
+
+workbox.precaching.precacheAndRoute(precacheManifest);
 
 workbox.routing.registerRoute(
-  /^\/api\//,
+  ({ request }) => request.mode === 'navigate',
   new workbox.strategies.NetworkFirst({
-    cacheName: 'api-cache',
-    networkTimeoutSeconds: 3,
-    plugins: [
-      new workbox.expiration.ExpirationPlugin({
-        maxEntries: 50,
-        maxAgeSeconds: 86400,
-      }),
-    ],
+    cacheName: 'html-cache'
+  })
+);
+
+workbox.routing.registerRoute(
+  /\.(?:png|jpg|jpeg|svg?)$/,
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: 'static-assets-cache',
   }),
   'GET'
 );
 
 workbox.routing.registerRoute(
-  ///^\/$/,
-  ({ request }) => request.mode === 'navigate',
-  new workbox.strategies.NetworkFirst({
-    cacheName: 'html-cache',
+  /^\/_next\/static\/.*/,
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: 'next-static-cache',
   }),
   'GET'
 );
--- workbox-config.cjs
+++ workbox-config.cjs
@@ -1,8 +1,15 @@
 module.exports = {
-	globDirectory: 'public/',
+	globDirectory: '.',
 	globPatterns: [
-		'**/*.{svg,json,js,ico}'
+		'public/**/*.{svg,json,js,ico}',
+    	'.next/static/**/*.{js,css,map}'
 	],
+	globIgnores: [
+    	'public/sw.js',       
+    	'app/sw.js',          
+    	'workbox-config.cjs',
+		'public/workbox*.js'
+  	],
 	swDest: 'public/sw.js',
 	swSrc: 'app/sw.js'
 };
