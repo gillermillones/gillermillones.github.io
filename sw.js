2d4b1afc58661b2ab47021167f0d5926d95c2ee5
gillermillones <159314465+gillermillones@users.noreply.github.com>
2025-12-17 19:34:52 +0100

    pages

---
+++ sw.js
@@ -0,0 +1,38 @@
+importScripts('/workbox-sw.js');
+
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', () => clients.claim());
+
+let precacheManifest = [{"revision":"5d855cc0ce9ce9546e654772a5b0adcd","url":"_not-found.html"},{"revision":"4226d08ebb29dcc5e4f22f529f839a6b","url":"workbox-sw.js"},{"revision":"201bca2347d3d4d4a72558e48e243bb8","url":"workbox-aa2f3006.js"},{"revision":"6b7eee73a1415d1695c8b2bd53858b00","url":"workbox-0859ddfe.js"},{"revision":"a2760511c65806022ad20adf74370ff3","url":"window.svg"},{"revision":"c0af2f507b369b085b35ef4bbe3bcf1e","url":"vercel.svg"},{"revision":"8e061864f388b47f33a1c3780831193e","url":"next.svg"},{"revision":"562adc8f60846db2ad9328fa3f71e25f","url":"manifest.json"},{"revision":"f78147db95ad3010f971298742f64bdc","url":"index.html"},{"revision":"2aaafa6a49b6563925fe440891e32717","url":"globe.svg"},{"revision":"d09f95206c3fa0bb9bd9fefabfd0ea71","url":"file.svg"},{"revision":"c30c7d42707a47a3f4591831641e50dc","url":"favicon.ico"},{"revision":"5d855cc0ce9ce9546e654772a5b0adcd","url":"404.html"},{"revision":"b404e23d62d95bafd03ad7747cc0e88b","url":"_next/static/yjI0PFaZBZTFCXETxfRwg/_ssgManifest.js"},{"revision":"d751713988987e9331980363e24189ce","url":"_next/static/yjI0PFaZBZTFCXETxfRwg/_clientMiddlewareManifest.json"},{"revision":"a02c1347626a3fb5fdfd040548ce5e9f","url":"_next/static/yjI0PFaZBZTFCXETxfRwg/_buildManifest.js"},{"revision":"020c3f98b7c1a902e935893eaedeb8a8","url":"_next/static/chunks/turbopack-59680dad9df20a9a.js"},{"revision":"3a939c72612905fee29a47c317adc9ee","url":"_next/static/chunks/fd5f064b2a3f9309.js"},{"revision":"db795236392d00f71763387d6f58f0bb","url":"_next/static/chunks/f39efe183210d53e.css"},{"revision":"d8cad7cb86f1c92aa32d7b2a16411130","url":"_next/static/chunks/c73ff364ca3634a4.js"},{"revision":"b9c3c3faef008dfb97fa62b6bde3eb98","url":"_next/static/chunks/b32a0963684b9933.js"},{"revision":"846118c33b2c0e922d7b3a7676f81f6f","url":"_next/static/chunks/a6dad97d9634a72d.js"},{"revision":"5fcc01f80632cedd173c6e7884e8c91b","url":"_next/static/chunks/42879de7b8087bc9.js"},{"revision":"a8502566f692c143b7b8013e22a4ae6c","url":"_next/static/chunks/26808309ccc2e230.js"},{"revision":"bc313444ee4f9217be36c8806de038ad","url":"_next/static/chunks/25393690ece2b925.js"}] || [];
+
+precacheManifest = precacheManifest.filter(entry => !entry.url.startsWith('.next/static/'));
+
+precacheManifest = precacheManifest.map(entry => ({
+  entry,
+  url: entry.url.replace(/^public\//, '/')
+}));
+
+workbox.precaching.precacheAndRoute(precacheManifest);
+
+workbox.routing.registerRoute(
+  ({ request }) => request.mode === 'navigate',
+  new workbox.strategies.NetworkFirst({
+    cacheName: 'html-cache'
+  })
+);
+
+workbox.routing.registerRoute(
+  /\.(?:png|jpg|jpeg|svg?)$/,
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: 'static-assets-cache',
+  }),
+  'GET'
+);
+
+workbox.routing.registerRoute(
+  /^\/_next\/static\/.*/,
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: 'next-static-cache',
+  }),
+  'GET'
+);
