b2ffcab85936d908240d831fe40895c5fa022284
gillermillones <titicht@hotmail.com>
2025-12-17 21:20:53 +0100

    funciona

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ function Home() {
     <html lang="en">
       <body>
         <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-          <main className="flex min-h-screen w-full max-w-3xl flex-col items-center py-32 px-16 bg-white dark:bg-black sm:items-start">
+          <main className="flex min-h-screen w-full max-w-3xl flex-col items-center py-15 px-5 bg-white dark:bg-black sm:items-start">
             <h1 className="mainTitle">Busqueda de libro en Google Books</h1>
             <Formulario/>
           </main>
