fc31e3f1b8f465fbef53bfb94899ffa6392b609b
gillermillones <gillermillones@users.noreply.github.com>
2025-12-17 19:08:40 +0000

    deploy: c330491fd090d50e1285ea6c696ce70ad03903ee

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  /* config options here */
-  output: 'export',
-  assetPrefix: '/gillermillones.github.io/',
-  basePath: '/gillermillones.github.io',
-};
-
-export default nextConfig;
